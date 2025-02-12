0f0d3227f6d680c23328e13b386e96fbd1a45ba2
Ryne VanKrevelen <rvankrevelen@elon.edu>
2025-02-12 17:14:03 -0500

    Small cosmetic changes

--- Retrosheet_Reading.R
+++ Retrosheet_Reading.R
@@ -11,14 +11,16 @@ useful_2024 <- csv_2024 %>%
   select("ab", "pa", "batter", "pitcher", "single" : "k")
 
 percentages_added <- useful_2024 %>%
-  mutate(bat_pa = n(), bat_kpct = sum(k) / bat_pa, 
+  mutate(bat_pa = n(), 
+         bat_kpct = sum(k) / bat_pa, 
          .by = batter) %>%
-  mutate(pit_pa = n(), pit_kpct = sum(k) / pit_pa, 
+  mutate(pit_pa = n(), 
+         pit_kpct = sum(k) / pit_pa, 
          .by = pitcher)
 
 ## Writing the CSV
 write.csv(percentages_added, 
           "percentages_added.csv", 
-          row.names=FALSE)
+          row.names = FALSE)
 
 
