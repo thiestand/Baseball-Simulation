be71a68b527214c609539da3d8ce9b9d40ef496e
Ryne VanKrevelen <rvankrevelen@elon.edu>
2025-02-19 14:02:42 -0500

    Updates to include samehand

--- Modeling with kpct's.R
+++ Modeling with kpct's.R
@@ -15,7 +15,7 @@ summary(model)
 # Predict for batter/pitcher combo
 
 predict(model, 
-        newdata= data.frame(bat_kpct = .30, pit_kpct = .17),
+        newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "no"),
         type = "response")
 
 
@@ -23,12 +23,16 @@ predict(model,
 
 pitch <- seq(0.05, 0.4, 0.05)
 bat <- seq(0.05, 0.4, 0.05)
+samehand <- c("yes", "no")
 
 data <- data.frame(pitch = pitch,
-                   bat = bat) |>
-  expand(pitch, bat) |>
+                   bat = bat,
+                   hand = samehand) |>
+  expand(pitch, bat, hand) |>
   mutate(k_pct = predict(model, 
-                         newdata = data.frame(bat_kpct = bat, pit_kpct = pitch),
+                         newdata = data.frame(bat_kpct = bat, 
+                                              pit_kpct = pitch,
+                                              samehand = hand),
                          type = "response"))
 
 ggplot(filter(data, pitch > 0.05), aes(x = pitch, y = bat, fill = k_pct)) +
@@ -40,10 +44,13 @@ ggplot(filter(data, pitch > 0.05), aes(x = pitch, y = bat, fill = k_pct)) +
        x = "Pitcher K%",
        y = "Batter K%",
        fill = "Predicted\nK%",
-       subtitle = "Based on logistic regression model")
+       subtitle = "Based on logistic regression model") +
+  facet_wrap(~hand)
 
 
 # Observed K% graph rounded to nearest 5% (at least 100 PAs)
+
+pa_min <- 100
 
 season_2024 |>
   filter(bat_pa >= pa_min,
@@ -52,7 +59,7 @@ season_2024 |>
          bat_kpct = round(bat_kpct * 20) / 20) |>
   summarize(k_rate = round(mean(k), 2),
             n = n(),
-            .by = c(pit_kpct, bat_kpct)) |>
+            .by = c(pit_kpct, bat_kpct, samehand)) |>
   filter(n > 100) |>
   ggplot(aes(x = pit_kpct, y = bat_kpct, fill = k_rate)) +
   geom_tile() +
@@ -63,4 +70,5 @@ season_2024 |>
        y = "Batter K %",
        fill = "Observed\nK%",
        subtitle = "Minimum 100 PAs for batter/pitcher K% combination") +
-  theme_minimal()
+  theme_minimal() +
+  facet_wrap(~samehand)
