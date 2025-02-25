2de6115f8dc059a190baee7de38ecb0de9035f7a
thiestand <159833415+thiestand@users.noreply.github.com>
2025-02-25 17:23:51 -0500

    2/26 Changes

--- Modeling Walks, Hits and Outs.R
+++ Modeling Walks, Hits and Outs.R
@@ -1,85 +1,106 @@
 # Creating and Modeling walks, hits and outs
+library(dplyr)
 
 season_2024 <- read.csv("percentages_added.csv")
 
 # Adding free_base variable and out variable
 season_2024 <- season_2024 |>
   mutate(free_base = hbp + walk) |>
-  mutate(out = case_when(free_base + single + double + triple + hr + k == 0 ~ 1,
+  mutate(ip_out = case_when(free_base + single + double + triple + hr + k == 0 ~ 1,
                          free_base + single + double + triple + hr + k == 1 ~ 0,
                          free_base + single + double + triple + hr + k > 1 ~ -1)) |>
-  mutate(adds = free_base + out + single + double + triple + hr + k)
+  mutate(adds = free_base + ip_out + single + double + triple + hr + k)
 
 # 1 or 0 determination
 table(season_2024$adds)
 
 
+season_2024 <- season_2024 |>
+  mutate(bat_fbpct = mean(free_base),
+         bat_1Bpct = mean(single),
+         bat_2Bpct = mean(double),
+         bat_3Bpct = mean(triple),
+         bat_HRpct = mean(hr),
+         bat_ipoutpct = mean(ip_out),
+         .by = batter) |>
+  mutate(pit_fbpct = mean(free_base),
+         pit_1Bpct = mean(single),
+         pit_2Bpct = mean(double),
+         pit_3Bpct = mean(triple),
+         pit_HRpct = mean(hr),
+         pit_ipoutpct = mean(ip_out),
+         .by = pitcher)
+
+write.csv(season_2024, 
+          "season_2024.csv", 
+          row.names=FALSE)
+
 #### Modeling each at-bat outcome
 
 # Free base model
 model_free_base <- season_2024 |>
-  glm(free_base ~ bat_kpct + pit_kpct + samehand,
+  glm(free_base ~ bat_fbpct + pit_fbpct + samehand,
       family = binomial,
       data = _)
 summary(model_free_base)
 
 pred_free_base <- predict(model_free_base, 
-        newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+        newdata= data.frame(bat_fbpct = .11, pit_fbpct = .07, samehand = "yes"),
         type = "response")
 
 # Single Model
 model_single <- season_2024 |>
-  glm(single ~ bat_kpct + pit_kpct + samehand,
+  glm(single ~ bat_1Bpct + pit_1Bpct + samehand,
       family = binomial,
       data = _)
 summary(model_single)
 
 pred_single <- predict(model_single, 
-        newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+        newdata= data.frame(bat_1Bpct = .15, pit_1Bpct = .12, samehand = "yes"),
         type = "response")
 
 # Double Model
 model_double <- season_2024 |>
-  glm(double ~ bat_kpct + pit_kpct + samehand,
+  glm(double ~ bat_2Bpct + pit_2Bpct + samehand,
       family = binomial,
       data = _)
 summary(model_double)
 
 pred_double <- predict(model_double, 
-                       newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+                       newdata= data.frame(bat_2Bpct = .02, pit_2Bpct = .06, samehand = "yes"),
                        type = "response")
 
 # Triple Model
 model_triple <- season_2024 |>
-  glm(triple ~ bat_kpct + pit_kpct + samehand,
+  glm(triple ~ bat_3Bpct + pit_3Bpct + samehand,
       family = binomial,
       data = _)
 summary(model_triple)
 
 pred_triple <- predict(model_triple, 
-                       newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+                       newdata= data.frame(bat_3Bpct = .01, pit_3Bpct = .01, samehand = "yes"),
                        type = "response")
 
 # Home Run Model
 model_hr <- season_2024 |>
-  glm(hr ~ bat_kpct + pit_kpct + samehand,
+  glm(hr ~ bat_HRpct + pit_HRpct + samehand,
       family = binomial,
       data = _)
 summary(model_hr)
 
 pred_hr <- predict(model_hr, 
-        newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+        newdata= data.frame(bat_HRpct = .02, pit_HRpct = .04, samehand = "yes"),
         type = "response")
 
 # Out Model
-model_out <- season_2024 |>
-  glm(out ~ bat_kpct + pit_kpct + samehand,
+model_ipout <- season_2024 |>
+  glm(out ~ bat_ipoutpct + pit_ipoutpct + samehand,
       family = binomial,
       data = _)
-summary(model_out)
+summary(model_ipout)
 
-pred_out <- predict(model_out, 
-        newdata= data.frame(bat_kpct = .30, pit_kpct = .17, samehand = "yes"),
+pred_ipout <- predict(model_ipout, 
+        newdata= data.frame(bat_ipoutpct = .50, pit_ipoutpct = .47, samehand = "yes"),
         type = "response")
 
 # K Model
@@ -96,9 +117,7 @@ pred_k <- predict(model_k,
 ### Exploration
 
 # Should equal 1?
-pred_single + pred_double + pred_triple + pred_hr + pred_free_base + pred_out + pred_k
-
-
+pred_single + pred_double + pred_triple + pred_hr + pred_free_base + pred_ipout + pred_k
 
 
 
--- sim_pa.R
+++ sim_pa.R
@@ -1,14 +1,26 @@
+season_2024 <- read.csv("season_2024.csv")
+
 sim_pa <- function(bat = "duraj001", pit = "canng001") {
   
   batter <- season_2024 |>
     filter(batter == bat) |>
     summarize(bat_kpct = mean(k), 
-              # add in bat_fbpct and all the others
+              bat_fbpct = mean(free_base),
+              bat_1Bpct = mean(single),
+              bat_2Bpct = mean(double),
+              bat_3Bpct = mean(triple),
+              bat_HRpct = mean(hr),
+              bat_ipoutpct = mean(ip_out),
               .by = bathand)
   pitcher <- season_2024 |>
     filter(pitcher == pit) |>
     summarize(pit_kpct = mean(k), 
-              # add in pit_fbpct and all the others
+              pit_fbpct = mean(free_base),
+              pit_1Bpct = mean(single),
+              pit_2Bpct = mean(double),
+              pit_3Bpct = mean(triple),
+              pit_HRpct = mean(hr),
+              pit_ipoutpct = mean(ip_out),
               .by = pithand)
   
   samehand <- batter$bathand == pitcher$pithand
@@ -16,16 +28,36 @@ sim_pa <- function(bat = "duraj001", pit = "canng001") {
   pred_k <- -3.91984 + 5.93173*batter$bat_kpct + 5.79395*pitcher$pit_kpct + 0.07264*samehand
   pred_k <- exp(pred_k) / (1 + exp(pred_k))
   
-  # calculate pred_fb and all the others using previous models
+  pred_fb <- -4.39809 + 11.42124*batter$bat_fbpct + 11.81275 *pitcher$pit_fbpct - 0.17712*samehand
+  pred_fb <- exp(pred_fb) / (1 + exp(pred_fb))
+  
+  pred_1B <- -4.19363  + 8.32839*batter$bat_1Bpct + 8.47410*pitcher$pit_1Bpct + 0.03093*samehand
+  pred_1B <- exp(pred_1B) / (1 + exp(pred_1B))
+
+  pred_2B <- -5.21054  + 24.99990*batter$bat_2Bpct + 24.16751*pitcher$pit_2Bpct - 0.08040*samehand
+  pred_2B <- exp(pred_2B) / (1 + exp(pred_2B))
+  
+  pred_3B <- -7.46952  + 170.10647*batter$bat_3Bpct + 183.24317*pitcher$pit_3Bpct - 0.06724*samehand
+  pred_3B <- exp(pred_3B) / (1 + exp(pred_3B))
+  
+  pred_HR <- -5.47323  + 32.11120*batter$bat_HRpct + 32.16880*pitcher$pit_HRpct - 0.13588*samehand
+  pred_HR <- exp(pred_HR) / (1 + exp(pred_HR))
+  
+  pred_ipout <- -5.47323  + 32.11120*batter$bat_ipoutpct + 32.16880*pitcher$pit_ipoutpct - 0.13588*samehand
+  pred_ipout <- exp(pred_ipout) / (1 + exp(pred_ipout))
   
   
-  # Bonus: Add up all the pred_xxxx. Then divide their values by the sum.
-  # pred_total <- pred_k + pred_fb + ...
-  # pred_k <- pred_k / pred_total
+  pred_total <- pred_k + pred_fb + pred_1B + pred_2B + pred_3B + pred_HR + pred_ipout
+  pred_k <- pred_k / pred_total
+  pred_fb <- pred_fb / pred_total
+  pred_1B <- pred_1B / pred_total
+  pred_2B <- pred_2B / pred_total
+  pred_3B <- pred_3B / pred_total
+  pred_HR <- pred_HR / pred_total
+  pred_ipout <- pred_ipout / pred_total
   
   print(batter)
   print(pitcher)
-  print(pred_k)
   
 }
 
